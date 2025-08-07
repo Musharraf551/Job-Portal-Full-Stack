a7c7b718b835095ea19857f1e559e7239d6851e3
musharraf <musharrafaleem2@gmail.com>
2025-08-07 21:16:38 +0530

    imported Home in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
 import Login from "./pages/Login";
 import Jobs from "./pages/Jobs";
 import MyApplications from "./pages/MyApplications";
