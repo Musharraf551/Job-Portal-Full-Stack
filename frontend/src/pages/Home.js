6e2d356c79e30de6cf153c125648c9539ff3f462
musharraf <musharrafaleem2@gmail.com>
2025-08-07 21:15:34 +0530

    added contents in Home.js inside src/pages

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Job Portal</h1>
+      <p>This is the home page of the Job Application System.</p>
+    </div>
+  );
+};
+
+export default Home;
