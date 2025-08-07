8e1deaf99f92a58cb1d655af2a315a6d2f3df1bf
musharraf <musharrafaleem2@gmail.com>
2025-08-07 21:48:42 +0530

    Added contents in Jobs.js

---
+++ frontend/src/pages/Jobs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Jobs() {
+  return (
+    <div>
+      <h2>Available Jobs</h2>
+      <p>List of all available job postings will go here.</p>
+    </div>
+  );
+}
+
+export default Jobs;
---
+++ frontend/src/pages/MyApplications.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyApplications() {
+  return (
+    <div>
+      <h2>My Applications</h2>
+      <p>Track the jobs you've applied to here.</p>
+    </div>
+  );
+}
+
+export default MyApplications;
